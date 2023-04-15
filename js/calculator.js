70ca4aaef31ebe06afaf2e9182fbdb4c39f1ae2b
Andrew <andreyshemet2003@gmail.com>
2023-04-15 20:52:49 +0300

    fixs

--- js/calculator.js
+++ js/calculator.js
@@ -1,2 +1,3 @@
-const userName = prompt('What is your name ?');
-alert('Nice to meet you '+userName);
+const hour = prompt("Введите сколько часов нужно конвертировать", 5);
+const second = hour * 60 * 60;
+alert(`Количество часов - ${hour}; после перевода в секунды - ${second}`)
