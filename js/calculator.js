a83a9425c575e0d5e118a2357433b2096318a718
Andrew <andreyshemet2003@gmail.com>
2023-04-17 00:37:32 +0300

    fixs1

--- js/calculator.js
+++ js/calculator.js
@@ -1,7 +1,25 @@
-const a = Number(prompt('what is the first number?'));
-const b = Number(prompt('what is the second number?'));
-let sum = a + b;
-let difference = a - b;
-let product = a * b;
-let quotient = a / b;
-alert (`${a} + ${b} = ${sum}\n${a} - ${b} = ${difference}\n${a} * ${b} = ${product}\n${a} / ${b} = ${quotient}`);
+let numOrStr = prompt('input number or string');
+/* if(numOrStr === null) {
+console.log('ви скасували')
+} else if( numOrStr.trim() === '' ) {
+console.log('Empty String');
+} else if ( isNaN( +numOrStr ) ) {
+console.log(' number is Ba_NaN')
+} else {
+console.log('OK!')
+} */
+
+switch (isNaN(numOrStr) || numOrStr) {
+	case  null:
+  	console.log('ви скасували')
+  break;
+  case  '':
+  	console.log('Empty String');
+  break;
+  case  true  :
+  	console.log(' number is Ba_NaN')
+  break;
+ 	default:
+    console.log('OK!')
+    break;
+}
