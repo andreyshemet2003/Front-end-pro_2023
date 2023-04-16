2cbf8cc50b40b6d55e592e03386792ce76655676
Andrew <andreyshemet2003@gmail.com>
2023-04-16 20:30:51 +0300

    fixs1

--- js/calculator.js
+++ js/calculator.js
@@ -1,4 +1,7 @@
-const hour = prompt("Введите сколько часов нужно конвертировать", 5);
-const second = hour * 60 * 60;
-alert(`Количество часов - ${hour}; после перевода в секунды - ${second}`)
-
+const a = Number(prompt('what is the first number?'));
+const b = Number(prompt('what is the second number?'));
+let sum = a + b;
+let difference = a - b;
+let product = a * b;
+let quotient = a / b;
+alert (`${a} + ${b} = ${sum}\n${a} - ${b} = ${difference}\n${a} * ${b} = ${product}\n${a} / ${b} = ${quotient}`);
