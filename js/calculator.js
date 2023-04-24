70ef410b0bf13d9425615e3e5b6a2e5d6b1f537c
Andrew <andreyshemet2003@gmail.com>
2023-04-24 16:54:06 +0300

    fixs2

--- js/calculator.js
+++ js/calculator.js
@@ -1,6 +1,16 @@
-const a = prompt('Введіть перше число:');
-const b = prompt('Введіть друге число:');
-const c = prompt('Введіть третє число:');
-const sum = (a + b + c) / 3;
+let OriginArray = prompt(`Уведіть кількість елементiв масиву:`);
+      let array = [];
+      for (i = 0, numIndex = 1; i < OriginArray; ++i, ++numIndex) {
+        array[i] = prompt(`Елемент № ${numIndex}:`);
+        console.log(`Елемент № ${numIndex}: ${array[i]}`);
+      }
+      console.log(`Отриманий масив: ${array}`)
 
-alert(`Середнє арифметичне: ${average}`);
+      function sor(a, b) {
+        return a - b; 
+      }
+      array.sort(sor);
+      console.log(`Сортування масиву за зростанням: ${array}`);
+
+      array.splice(1, 3);
+      console.log(`Відсортований масив з видаленими елементами з 2 по 4: ${array}`);
