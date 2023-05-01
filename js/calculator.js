f27cd93a38463d59a5d055d4d61239df25176881
Andrew <andreyshemet2003@gmail.com>
2023-05-01 12:45:17 +0300

    fixs2

--- js/calculator.js
+++ js/calculator.js
@@ -1,9 +1,8 @@
-function removeElement(array, item) {
-  const index = array.indexOf(item);
-  if (index !== -1) {
-      array.splice(index, 1);
-  }
-}
-const array = [1, 2, 3, 4, 5];
-removeElement(array, 3);
-console.log(array);
+let alphabet = 'abcdefghijklmnopqrstuvwxyz0123456789';
+let key = '';
+
+Array.from({ length: 16 }).forEach(() => {
+    key += alphabet[Math.round(Math.random() * (alphabet.length - 1))];
+});
+
+console.log(key);
