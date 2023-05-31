2efac52963f64112a4a13dcb88b75e87f7e15dee
Andrew <andreyshemet2003@gmail.com>
2023-05-31 17:14:24 +0300

    fixs1

--- js/list.js
+++
@@ -1,9 +0,0 @@
-export default function generateList(student, togleTrueOrFalse = 0) {
-    for (let i = 0; i < student.visitList.length; i++) {
-        if (Math.round(Math.random() + togleTrueOrFalse)) {
-            student.present();
-        } else {
-            student.absent();
-        }
-    }
-}
--- js/script.js
+++ js/script.js
@@ -1,25 +1,19 @@
+const body = document.querySelector('body');
+const table = document.createElement('table');
 
-import generateList from "./list.js";
-import Student from "./student.js";
+body.insertBefore(table, body.lastChild);
+const row = prompt('Input quantity rows');
+const columns = prompt('Input quantity culumns');
+let counter = 1;
 
-// function generateList(student, togleTrueOrFalse(1: only True, -1: only False, default: random)
-let student1 = new Student("Sasha", "Gontarev", 1981, [85, 80, 80, 78, 55]);
-console.log(student1);
-console.log("Age: ", student1.ageStudent());
-console.log("Average grade: ", student1.averageScore());
-generateList(student1);
-console.log(student1.summary())
 
-let student2 = new Student("Anna", "Gontareva", 1985, [90, 90, 85, 88, 99]);
-console.log(student2);
-console.log("Age: ", student2.ageStudent());
-console.log("Average grade: ", student2.averageScore());
-generateList(student2, 1);
-console.log(student2.summary())
+Array.from({ length: row }).map(() => {
+    const tr = document.createElement('tr');
+    table.append(tr);
 
-let student3 = new Student("Dmytro", "Gontarev", 2010, [100, 90, 95, 92, 97]);
-console.log(student3);
-console.log("Age: ", student3.ageStudent())
-console.log("Average grade: ", student3.averageScore())
-generateList(student3)
-console.log(student3.summary())
+    Array.from({ length: columns }).map(() => {
+        const td = document.createElement('td');
+        tr.append(td);
+        td.innerText = `${counter++}`;
+    });
+});
